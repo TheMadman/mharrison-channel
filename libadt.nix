b259a0d2df4bbc1200b48ed5f5a0bf12420936f9
Marcus Harrison <marcus@harrisonland.co.uk>
2026-08-30 20:04:47 +0200

    Update libadt

--- libadt.nix
+++ libadt.nix
@@ -4,8 +4,8 @@ stdenv.mkDerivation {
 	version = "0.0.1";
 	src = fetchFromRepo {
 		repo = "libadt";
-		rev = "6be08777cb39a84e81b74ce1591a74558ae9c630";
-		hash = "sha256-+rcZ5+IbsWjD9AjWxEMM3PWLWonPnc25hbk4UPHOtlU=";
+		rev = "c8491c5c9a7398854078c0ba3b821931f39645ae";
+		hash = "sha256-6c+vUYye9s2YZFEgOLorr9znUfV6acqi8Q9s16tQxOQ=";
 	};
 
 	nativeBuildInputs = [cmake];
