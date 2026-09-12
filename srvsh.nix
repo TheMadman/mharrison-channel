ece4673a91188aed934a5fabf6da68c7f8cb2929
Marcus Harrison <marcus@harrisonland.co.uk>
2026-09-12 21:20:56 +0200

    Update srvsh

--- srvsh.nix
+++ srvsh.nix
@@ -2,10 +2,11 @@
 stdenv.mkDerivation {
 	pname = "srvsh";
 	version = "0.0.1";
+
 	src = fetchFromRepo {
 		repo = "srvsh";
-		rev = "100d8c91b23e6022de20953ee2f973a0fb32c4df";
-		hash = "sha256-8tWYWT0JMPKbbYlxBpQyRb/4Q1mhJt5/vdZhEGh/4uY=";
+		rev = "d56e47fdf76a11fab027e2f19f7be1969335252b";
+		hash = "sha256-k6g7JKToRds5kvMMRGqGcVRHSL7hiS8XWaK93e0BUDo=";
 	};
 
 	buildInputs = [
